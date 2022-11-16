68f607888efc9f373515c554bc87d78734a01a9d
GayuRaju <112641723+GayuRaju@users.noreply.github.com>
2022-11-16 21:16:20 +0530

    Updating application code

--- Project Development phase/Sprint3/src/components/Skill.jsx
+++ Project Development phase/Sprint3/src/components/Skill.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { TiTick } from "react-icons/ti";
+import React, { useEffect, useState } from "react";
 
-const Skill = ({ skill, checked }) => {
+const Skill = ({ skill, setSelectedSkills, disabled }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  useEffect(() => {
+    if (isSelected) {
+      setSelectedSkills((prev) => [...prev, skill]);
+    } else {
+      setSelectedSkills((prev) => prev.filter((item) => item !== skill));
+    }
+  }, [isSelected]);
+
   return (
-    <li className="hover:text-white cursor-pointer flex gap-1 items-center">
+    <li className="hover:text-white flex gap-1 items-center justify-between p-1 rounded-sm">
       {skill}
-      {checked && <TiTick />}
+      <button
+        disabled={disabled}
+        onClick={() => setIsSelected(!isSelected)}
+        className={`cursor-pointer border-2 ${
+          !isSelected ? "border-green-500" : "border-red-400"
+        } p-1 rounded-lg`}
+      >
+        {!isSelected ? "Add" : "Remove"}
+      </button>
     </li>
   );
 };
