87ae1ccbac2769906192ce2434cdee453e75aca6
GayuRaju <112641723+GayuRaju@users.noreply.github.com>
2022-11-16 22:01:32 +0530

    Updating application code

--- Project Development phase/Sprint4/src/screens/Dashboard.jsx
+++ Project Development phase/Sprint4/src/screens/Dashboard.jsx
@@ -1,6 +1,11 @@
+import {
+  Progress,
+  SkeletonCircle,
+  SkeletonText,
+  Spinner,
+} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import JobCard from "../components/JobCard";
 import SearchBar from "../components/SearchBar";
 import Skill from "../components/Skill";
@@ -10,85 +15,144 @@ import { getUserSkills } from "../proxies/backend_api";
 const Dashboard = () => {
   const { user, skills, setSkills } = useContext(AppContext);
 
-  const [filterUsingSkills, setFilterUsingSkills] = useState(false);
+  const [selectedSkills, setSelectedSkills] = useState([]);
+
+  const [skillsLoading, setSkillsLoading] = useState(false);
+
+  const [jobsLoading, setJobsLoading] = useState(false);
 
   const [query, setquery] = useState("");
-  const [posts, setposts] = useState(null);
+
+  const [posts, setPosts] = useState(null);
+
   const id = import.meta.env.VITE_ADZUNA_API_ID;
+
   const key = import.meta.env.VITE_ADZUNA_API_KEY;
-  const baseURL = `http://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${id}&app_key=${key}&results_per_page=15&what=${query}&what_and=${skills.join(
+
+  const baseURL_with_skills = `http://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${id}&app_key=${key}&results_per_page=15&what=${query}&what_and=${selectedSkills.join(
     " "
   )}&&content-type=application/json`;
 
-  const baseURL2 = `http://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${id}&app_key=${key}&results_per_page=15&what=${query}&content-type=application/json`;
+  const baseURL = `http://api.adzuna.com/v1/api/jobs/in/search/1?app_id=${id}&app_key=${key}&results_per_page=15&what=${query}&content-type=application/json`;
 
   const searchJobsFromQuery = async () => {
-    const { data } = await axios.get(filterUsingSkills ? baseURL : baseURL2);
-    setposts(data.results);
+    setJobsLoading(true);
+
+    if (query !== "" || !posts) {
+      const { data } = await axios.get(baseURL);
+      setPosts(data.results);
+    }
+
+    setJobsLoading(false);
+  };
+
+  const searchWithSkills = async () => {
+    setJobsLoading(true);
+
+    const { data } = await axios.get(baseURL_with_skills);
+
+    setPosts(data.results);
+
+    setJobsLoading(false);
   };
 
   useEffect(() => {
     if (user) {
       (async () => {
+        setSkillsLoading(true);
         setSkills(await getUserSkills(user.token));
+        setSkillsLoading(false);
       })();
     }
   }, [user]);
 
   useEffect(() => {
+    searchWithSkills();
+  }, [selectedSkills]);
+
+  useEffect(() => {
     searchJobsFromQuery();
-  }, [filterUsingSkills]);
+  }, []);
 
   return (
-    <div className="flex gap-10 my-10 lg:my-24 mx-20 lg:mx-40">
-      <div className="hidden lg:block bg-primary w-1/5 p-10 h-3/6 rounded-lg">
-        <div className="text-xl text-white capitalize font-extrabold mb-6">
-          Your skills
-        </div>
-        <ul className="list-none text-gray-200 flex flex-col gap-2">
-          {skills.map((skill, ind) => (
-            <Skill skill={skill} key={ind} checked={filterUsingSkills} />
-          ))}
-        </ul>
-        <button
-          className="p-2 bg-white text-primary rounded mt-5"
-          onClick={() => {
-            setFilterUsingSkills(!filterUsingSkills);
-            searchJobsFromQuery();
-          }}
-        >
-          Include your skills
-        </button>
-      </div>
-
-      <div className="mx-auto">
-        <SearchBar setquery={setquery} onClick={searchJobsFromQuery} />
-        {query === "" ? (
-          <h2 className="text-2xl mt-5">Recommended Jobs</h2>
-        ) : (
-          <h2 className="text-2xl mt-5">
-            Search for keywords {query}
-            {filterUsingSkills && `,${skills.join(",")}`}
-          </h2>
-        )}
-
-        <div className="mt-10 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
-          {query !== null ? (
-            posts?.map((post, ind) => (
-              <JobCard
-                key={ind}
-                title={post.title}
-                company={post.company.display_name}
-                description={post.description}
-                link={post.redirect_url}
-              />
-            ))
+    <>
+      {(jobsLoading || skillsLoading) && (
+        <Progress size="xs" isIndeterminate colorScheme={"purple"} />
+      )}
+      <div className="flex gap-10 m-10">
+        <div className="hidden lg:flex bg-purple-600 w-1/5 p-5 h-3/6 rounded-lg text-center flex-col gap-4">
+          <div className="text-2xl text-white capitalize font-extrabold">
+            Your skills
+          </div>
+          {skillsLoading ? (
+            <Spinner
+              className="self-center my-5"
+              thickness="3px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="black.100"
+              size="lg"
+            />
           ) : (
-            <></>
+            <ul className="list-none text-gray-200 flex flex-col gap-2">
+              {skills?.length === 0 ? (
+                <p className="text-gray-300">
+                  Skills you add in the profile section will appear here!!
+                </p>
+              ) : (
+                skills.map((skill, ind) => (
+                  <Skill
+                    skill={skill}
+                    key={ind}
+                    setSelectedSkills={setSelectedSkills}
+                    disabled={skillsLoading}
+                  />
+                ))
+              )}
+            </ul>
           )}
+          <p className="text-white text-sm">
+            (Include your skills in the search result)
+          </p>
+        </div>
+
+        <div className="mx-auto min-w-[80%] ">
+          <SearchBar setquery={setquery} onClick={searchJobsFromQuery} />
+          {query === "" ? (
+            <h2 className="text-2xl mt-5">Recommended Jobs</h2>
+          ) : (
+            <h2 className="text-2xl mt-5">
+              Search for keywords {query}
+              {filterUsingSkills && `,${skills.join(",")}`}
+            </h2>
+          )}
+
+          <div className="mt-5 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5">
+            {jobsLoading
+              ? [...new Array(10)].map((_, ind) => (
+                  <div key={ind}>
+                    <SkeletonCircle size="8" className="mb-5" />
+                    <SkeletonText
+                      mt="4"
+                      noOfLines={8}
+                      spacing="4"
+                      color={"red"}
+                    />
+                  </div>
+                ))
+              : posts?.map((post, ind) => (
+                  <JobCard
+                    key={ind}
+                    title={post.title}
+                    company={post.company.display_name}
+                    description={post.description}
+                    link={post.redirect_url}
+                  />
+                ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
